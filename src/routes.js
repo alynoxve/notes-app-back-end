5554b27d745580d327c3c844fc874bb958f66d45
alynoxve <alimaulana490@gmail.com>
2022-06-13 09:42:29 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+import {
+    addNoteHandler,
+    getAllNotesHandler,
+    getNotesByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler,
+} from "./handler.js";
+
+const routes = [
+    {
+        method : 'POST',
+        path : '/notes',
+        handler : addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNotesByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method : 'DELETE',
+        path : '/notes/{id}',
+        handler : deleteNoteByIdHandler,
+    },
+];
+
+export default routes;
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import { server as _server } from '@hapi/hapi';
+import routes from './routes.js';
+
+const init = async () => {
+    const server = _server({
+        port : 5000,
+        host : 'localhost',
+        routes: {
+            cors: {
+              origin: ['*'],
+            },
+        },
+    });
+    server.route(routes);
+    
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
